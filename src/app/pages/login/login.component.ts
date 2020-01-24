d9ea3ad0ade77afcd1e6a8ca1ee0fc39b19af108
gurram.navyasri91@gmail.com <gurram.navyasri91@gmail.com>
2020-01-24 15:43:16 +0530

    aded auth service and authguard changes

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { AuthService } from '../../auth.service';
+import { UserProfileModel } from '../../models/user_profile.model';
 
 @Component({
   selector: 'app-login',
@@ -7,14 +9,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  constructor(private router:Router) {}
+
+  constructor(private router: Router,
+    private _authService: AuthService) {
+  }
+
+  userInfo:UserProfileModel;
 
   ngOnInit() {
+
+    this.userInfo = new UserProfileModel();
   }
   ngOnDestroy() {
   }
 
-  signInOkClick() {
+  async signInOkClick() {
+
+    await this._authService.auth(this.userInfo.emailId, this.userInfo.password);
+
     this.router.navigate(['/dashboard']);
   }
 
